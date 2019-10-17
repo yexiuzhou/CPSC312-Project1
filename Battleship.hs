53a10861e14eccba0c887aeecabcb47c594a88da
isaac cheung <isaac.cheung@hotmail.com>
2019-10-16 18:01:07 -0700

    Update Battleship.hs

--- Battleship.hs
+++ Battleship.hs
@@ -1,15 +1,18 @@
-type Coordinate = (Int, Int)
+type Coordinate = (Char, Int)
 type Ship = [Coordinate]
 type Field = [[Bool]]
 
+validX = ['A' .. 'J']
+validY = [1 .. 10]
 -- Probably also want a player type
 
 -- Initialize the play field
--- LOL hexagonal field - gotta figure out how hashmaps or something work in Haskell
 initField :: Field
 
 -- Extract the coordinate from the string
 convertInputToCoords :: String -> Coordinate
+
+convertInputToCoords h:t = (h,t) -- need to convert t to Int somehow
 
 -- Split a string containing coordinates separated by semi-colons into a list of coordinates
 splitCoordsInString :: String -> [String]
@@ -17,11 +20,22 @@ splitCoordsInString :: String -> [String]
 -- Check if a coordinate lies inside the field
 validateCoords :: Coordinate -> Bool
 
+validateCoords (x,y) = (elem x validX) && (elem y validY)
+
 -- Make sure that the ship is given valid coordinates (in a row/col, no overlap with existing ships)
 -- ie for a ship 3 long, make sure it receives 3 coords that are in the same "row"
 -- might want to have data about "rows" when we initialize the field
 -- input: already placed ships, current ship (coords), length of current ship
 validateShipCoordinates :: [Ship] -> Ship -> Int -> Bool
+
+validateShipCoordinates h:t ship len = (validShip h ship len) && (validateShipCoordinates t ship len)
+
+-- validShip is a helper function for validateShipCoordinates that makes sure the 2 given ships do not 
+-- inhabit the same coordinates and ensures the second ship is given valid coordinates
+-- input: already placed ship, current ship, length of current ship
+validShip :: Ship -> Ship -> Int -> Bool
+
+validShip = 
 
 -- Output the field in the terminal (as some printable String)
 printField :: Field -> [Ship] -> IO ()
@@ -53,5 +67,35 @@ inputShips :: Int -> [Ship] -> IO [Ship]
 -- Input the names of the player (computer will always be "Computer")
 inputNames :: IO [String]
 
+
+
 -- The entry point of the program
 main :: IO ()
+
+main =
+	do 
+		putStrLn("Welcome to battleship in haskell, let's get started... ")
+        putStrLn("Would you like to start a new game or load a pre-existing file?")
+        putStrLn("[1] - New Game, [2] - Load Game")
+        newOrLoad <- getLine
+
+        if (newOrLoad == "1") -- new game
+        	then do
+        		putStrLn("Ok, lets start a new game.")
+        		putStrLn("First off lets set up your board")
+
+        else do -- load game
+      		putStrLn("What is your file's name?")
+      		fileName <- getLine
+      		file <- readFile fileName
+
+
+
+
+
+
+
+
+
+
+
